e5bd6fc8d518fed8202c821abac07a4aa218af2f
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-07-26 20:07:56 +0530

    Level indicator

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function checkAnswer(currentLevel){
         setTimeout(function(){
           $("body").removeClass("game-over");
         },200);
-        $("h1").text("Game Over, (Press Any Key to Restart)");
+        $("h1").text("Game Over, (Level : "+(level)+") (Press Any Key to Restart)");
         startOver();
     }
     }
